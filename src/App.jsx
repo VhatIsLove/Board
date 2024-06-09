680c28a040863737e5b19a0c7ef0ff32c3138cce
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2024-06-09 14:04:52 +0300

    init: start of project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Vite + React</h1>
+     
+    </>
+  )
+}
+
+export default App
