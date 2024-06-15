90a08c3a04e6b7c739d8ab2ff0779880ea835aed
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2024-06-15 14:21:28 +0300

    feat: completed transfer task

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,19 @@ import Header from './components/Header';
 const taskStatus = ['todo', 'progress', 'review', 'done']
 
 const initialTask = [
-	{id: 1, title: 'Задача 1', status: 'todo', description: 'Написать приложение с использованием React', priority: 1},
-	{id: 2, title: 'Задача 2', status: 'progress', description: 'Продумать логику доски задач', priority: 3}
+	{id: 1, title: 'Задача 1', status: 'done', description: 'Начать новый проект', priority: 1},
+	{id: 2, title: 'Задача 2', status: 'progress', description: 'Продумать логику доски задач', priority: 3},
+	{id: 3, title: 'Задача 3', status: 'todo', description: 'Написать приложение с использованием React', priority: 1},
+	{id: 4, title: 'Задача 4', status: 'review', description: 'Добавить стили и сделать красоту', priority: 4}
 ]
 
 function App() {
-	const [tasks, setTasks] = useState(initialTask)
+	const [tasks, setTasks] = useState(initialTask);
+
+	//проходим по всем таскам в 'useState', затем проверяем 'id' каждого элемента и если он равен тому что в событии 'onChange'  в 'select', тогда возвращаем новый объект
+	const oneStatusChange = (id, newStatus) => {
+		setTasks(tasks.map((task) => (task.id === id) ?  {...task, status: newStatus} : task));
+	}
 
   return (
       <div className='container'>
@@ -23,10 +30,14 @@ function App() {
 
 				<div className='row'>
 					{taskStatus.map((status) =>(
-						<Column key={status} status={status} tasks={tasks} />
+						<Column 
+							key={status} 
+							status={status} 
+							tasks={tasks} 
+							statuses={taskStatus}
+							oneStatusChange={oneStatusChange}
+						/>
 					))}
-					
-
 				</div>
 
       </div>)
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,12 +4,30 @@ export default function Column(props){
 		<div className="col">
 
 		<h3>{props.status}</h3>
-		{props.tasks.map((task) => (
+		{props.tasks
+		.filter((task) => task.status === props.status)
+		.map((task) => (
 			<div className="card mb-3" key={task.id}>
 				<div className="card-body">
 					<h5 className="card-title">{task.title}</h5>
 					<p>{task.status}</p>
 					<p>{task.description}</p>
+
+					<select 
+						className="form-select" 
+						aria-label="Default select example"
+						defaultValue={props.status}
+						onChange={(e) => props.oneStatusChange(task.id, e.target.value)}>
+
+							{props.statuses.map((status) => (
+								<option 
+									key={status} 
+									value={status}>
+										{status}
+									</option>
+							))}
+
+					</select>
 				</div>
 			</div>
 		))}
