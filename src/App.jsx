d374e27d0d64979c92ce0757a770bb7436cdbc19
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2024-06-15 12:53:54 +0300

    feat: connected bootstrap and set up the speakers

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import './App.scss'
 import Column from './components/Column';
 import Header from './components/Header';
 
-// const taskStatus = ['todo', 'progress', 'review', 'done']
+const taskStatus = ['todo', 'progress', 'review', 'done']
 
 const initialTask = [
-	{id: 1, title: 'Задача 1', status: 'todo', description: 'Описание задачи 1', priority: 1},
-	{id: 2, title: 'Задача 2', status: 'progress', description: 'Описание задачи 2', priority: 3}
+	{id: 1, title: 'Задача 1', status: 'todo', description: 'Написать приложение с использованием React', priority: 1},
+	{id: 2, title: 'Задача 2', status: 'progress', description: 'Продумать логику доски задач', priority: 3}
 ]
 
 function App() {
 	const [tasks, setTasks] = useState(initialTask)
-	console.log(initialTask);
+
   return (
-      <div >
+      <div className='container'>
+
 			<Header />
+
         <h1>Hello board</h1>
-				<Column tasks={tasks} />
+
+				<div className='row'>
+					{taskStatus.map((status) =>(
+						<Column key={status} status={status} tasks={tasks} />
+					))}
+					
+
+				</div>
+
       </div>)
 }
 
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,13 +1,16 @@
 // через 'props' получаем значение 'tasks'
 export default function Column(props){
 	return (
-		<div className="column">
+		<div className="col">
 
+		<h3>{props.status}</h3>
 		{props.tasks.map((task) => (
-			<div className="task" key={task.id}>
-				<h3>{task.title}</h3>
-				<p>{task.status}</p>
-				<p>{task.description}</p>
+			<div className="card mb-3" key={task.id}>
+				<div className="card-body">
+					<h5 className="card-title">{task.title}</h5>
+					<p>{task.status}</p>
+					<p>{task.description}</p>
+				</div>
 			</div>
 		))}
     
